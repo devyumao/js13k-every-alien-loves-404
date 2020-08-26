e1eaa2462e34ab36de526920920ab4966af97dd0
devyumao <yuzhang.lille@gmail.com>
2020-08-26 21:42:00 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,91 @@
+var renderer, scene, camera;
+var earth, ufo;
+var keys = [];
+
+main();
+
+function main() {
+    initScene();
+
+    createEarth();
+    createUfo();
+
+    initRenderer();
+
+    window.addEventListener('resize', onWindowResize, false);
+    document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener('keyup', onKeyUp, false);
+
+    animate();
+}
+
+function initScene() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    camera.position.z = 20;
+
+    var light = new THREE.AmbientLight(0xffffff); 
+    scene.add(light);
+}
+
+function initRenderer() {
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
+
+function createEarth() {
+    var geometry = new THREE.SphereGeometry(10, 64, 64);
+    var textureLoader = new THREE.TextureLoader();
+    textureLoader.load('./asset/map.jpg', texture => {  
+        var material = new THREE.MeshLambertMaterial({ map: texture, transparent: true });
+        earth = new THREE.Mesh(geometry, material);  
+        scene.add(earth);
+    });
+}
+
+function createUfo() {
+    var geometry = new THREE.ConeGeometry(0.5, 0.25, 32);
+    var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    ufo = new THREE.Mesh(geometry, material);
+    ufo.position.set(0, 2, 11);
+    ufo.rotation.x = 1
+    scene.add(ufo);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function onKeyDown(e) {
+    keys[e.keyCode] = true;
+}
+
+function onKeyUp(e) {
+    keys[e.keyCode] = false;
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    if (earth) {
+        // TODO: 搞点惯性
+        if (keys[87] /* W */ || keys[38] /* ArrowUp */) {
+            earth.rotation.x += 0.01;
+        }
+        if (keys[83] /* S */ || keys[40] /* ArrowDown */) {
+            earth.rotation.x -= 0.01;
+        }
+        if (keys[65] /* A */ || keys[37] /* ArrowLeft */) {
+            earth.rotation.y += 0.01;
+        }
+        if (keys[68] /* D */ || keys[39] /* ArrowRight */) {
+            earth.rotation.y -= 0.01;
+        }
+    }
+
+    renderer.render(scene, camera);
+}
