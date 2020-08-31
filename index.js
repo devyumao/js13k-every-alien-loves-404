1dd45151335b171a1a1ce97c2409828d671ecfe2
devyumao <yuzhang.lille@gmail.com>
2020-08-31 23:38:05 +0800

    feat: disable camera movement

--- index.js
+++ index.js
@@ -501,8 +501,8 @@ function updateUfoRotation() {
     ufo.rotation.x = ufoOriginRotation.x - angularVel.phi * 30;
     ufo.rotation.z = ufoOriginRotation.z + angularVel.theta * 30;
 
-    camera.position.y = -angularVel.phi * 50;
-    camera.position.x = angularVel.theta * 50;
+    // camera.position.y = -angularVel.phi * 50;
+    // camera.position.x = angularVel.theta * 50;
 }
 
 function updateUfoActions() {
